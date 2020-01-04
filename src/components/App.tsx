588bef5491e4e347a8c232ea977d3e939cb29502
Andrew Huth <andrew@huth.me>
2020-01-03 22:13:17 -0800

    Draw some basic shapes to the canvas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export default function App() {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const context = canvasRef.current.getContext('2d');
+
+      if (context) {
+        context.beginPath();
+        context.rect(20, 40, 50, 50);
+        context.fillStyle = '#FF0000';
+        context.fill();
+        context.closePath();
+
+        context.beginPath();
+        context.arc(240, 160, 20, 0, Math.PI * 2, false);
+        context.fillStyle = 'green';
+        context.fill();
+        context.closePath();
+      }
+    }
+  }, []);
+
   return <canvas height={320} width={480} ref={canvasRef} />;
 }

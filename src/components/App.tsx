ce505b589da73189f4a1f3363c6dce14fb17d142
Andrew Huth <andrew@huth.me>
2020-01-03 22:34:08 -0800

    Re-arrange guard clauses to reduce nesting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,38 +4,36 @@ export default function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
+    if (!canvasRef.current) { return; }
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    if (!context ) { return; }
 
-      if (context) {
-        let x = canvas.width / 2;
-        let y = canvas.height - 30;
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
 
-        const dx = 2;
-        const dy = -2;
+    const dx = 2;
+    const dy = -2;
 
-        const drawBall = () => {
-          context.beginPath();
-          context.arc(x, y, 10, 0, Math.PI * 2);
-          context.fillStyle = '#0095DD';
-          context.fill();
-          context.closePath();
-        };
+    const drawBall = () => {
+      context.beginPath();
+      context.arc(x, y, 10, 0, Math.PI * 2);
+      context.fillStyle = '#0095DD';
+      context.fill();
+      context.closePath();
+    };
 
-        const draw = () => {
-          context.clearRect(0, 0, canvas.width, canvas.height);
-          drawBall();
+    const draw = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      drawBall();
 
-          x += dx;
-          y += dy;
+      x += dx;
+      y += dy;
 
-          window.requestAnimationFrame(draw);
-        };
+      window.requestAnimationFrame(draw);
+    };
 
-        window.requestAnimationFrame(draw);
-      }
-    }
+    window.requestAnimationFrame(draw);
   }, []);
 
   return <canvas height={320} width={480} ref={canvasRef} />;

c5ca67c0c4192aa16d71c3032a6fe09e259ec5b1
Andrew Huth <andrew@huth.me>
2020-01-03 22:06:15 -0800

    Add canvas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function App() {
-  return <h1>hello world</h1>;
+  const canvasRef = useRef(null);
+  return <canvas height={320} width={480} ref={canvasRef} />;
 }

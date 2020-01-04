53e45c8ce0595a7066656a137a8685f6ccbeb6d0
Andrew Huth <andrew@huth.me>
2020-01-03 22:37:48 -0800

    Bounce the ball off walls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,12 +12,13 @@ export default function App() {
     let x = canvas.width / 2;
     let y = canvas.height - 30;
 
-    const dx = 2;
-    const dy = -2;
+    const ballRadius = 10;
+    let dx = 2;
+    let dy = -2;
 
     const drawBall = () => {
       context.beginPath();
-      context.arc(x, y, 10, 0, Math.PI * 2);
+      context.arc(x, y, ballRadius, 0, Math.PI * 2);
       context.fillStyle = '#0095DD';
       context.fill();
       context.closePath();
@@ -26,6 +27,13 @@ export default function App() {
     const draw = () => {
       context.clearRect(0, 0, canvas.width, canvas.height);
       drawBall();
+
+      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+      }
+      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+      }
 
       x += dx;
       y += dy;

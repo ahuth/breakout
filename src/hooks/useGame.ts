857cadf3320805f3b853af5b9f1a535aaecccacc
Andrew Huth <andrew@huth.me>
2020-01-04 18:11:11 -0800

    Allow the user to move the paddle

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -5,16 +5,37 @@ export default function useGame(canvasRef: React.RefObject<HTMLCanvasElement>): 
     const canvas = canvasRef.current!;
     const context = canvas.getContext('2d')!;
 
+    let rightPressed = false;
+    let leftPressed = false;
+
     let x = canvas.width / 2;
     let y = canvas.height - 30;
 
     const paddleHeight = 10;
     const paddleWidth = 75;
-    const paddleX = (canvas.width - paddleWidth) / 2;
+    let paddleX = (canvas.width - paddleWidth) / 2;
 
     const ballRadius = 10;
     let dx = 2;
     let dy = -2;
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+      }
+      else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+      }
+    };
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+      }
+      else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+      }
+    };
 
     const drawBall = () => {
       context.beginPath();
@@ -48,9 +69,29 @@ export default function useGame(canvasRef: React.RefObject<HTMLCanvasElement>): 
       x += dx;
       y += dy;
 
+      if (rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width) {
+          paddleX = canvas.width - paddleWidth;
+        }
+      } else if (leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0) {
+          paddleX = 0;
+        }
+      }
+
       window.requestAnimationFrame(draw);
     };
 
+    document.addEventListener('keydown', handleKeyDown, false);
+    document.addEventListener('keyup', handleKeyUp, false);
+
     window.requestAnimationFrame(draw);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown, false);
+      document.removeEventListener('keyup', handleKeyUp, false);
+    };
   }, [canvasRef]);
 }
